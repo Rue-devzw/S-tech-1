feef03797861743fbc5d2d118fddc7858a1d485a
Rufaro Bernards <rufarobernards@gmail.com>
2025-10-15 10:21:08 +0000

    Error: ReactDOM.useFormState has been renamed to React.useActionState. P

--- src/app/repairs/repair-quote-form.tsx
+++ src/app/repairs/repair-quote-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useFormState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { submitRepairQuote } from '../actions';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
@@ -34,7 +35,7 @@ function SubmitButton() {
 }
 
 export default function RepairQuoteForm() {
-  const [state, formAction] = useFormState(submitRepairQuote, initialState);
+  const [state, formAction] = useActionState(submitRepairQuote, initialState);
 
   return (
     <div className="space-y-6">
