65da950bf13dbbf2f0a595ca481b1ec234aa946b
Rufaro Bernards <rufarobernards@gmail.com>
2025-10-15 10:20:26 +0000

    Error: React does not recognize the `dM` prop on a DOM element. If you i

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -16,7 +16,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
     >
       <path d="M12.5 6.5L15.5 4L19 7.5L16 10" />
       <path d="M15.5 4L9.5 10" />
-      <path dM="M18 10L12 16" />
+      <path d="M18 10L12 16" />
       <path d="M4 14L10 20" />
       <path d="M8 12L14 18" />
       <path d="M13.5 17.5L10.5 20L7 16.5L10 14" />
